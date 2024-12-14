cf1e3ddd498a838d8e423e9ce14ee45630a94f9e
Paul Beck <sparkfox23@mailbox.org>
2024-12-13 22:17:20 -0500

    Disable clickable blocks

--- config.h
+++ config.h
@@ -8,7 +8,7 @@
 #define MAX_BLOCK_OUTPUT_LENGTH 45
 
 // Control whether blocks are clickable.
-#define CLICKABLE_BLOCKS 1
+#define CLICKABLE_BLOCKS 0
 
 // Control whether a leading delimiter should be prepended to the status.
 #define LEADING_DELIMITER 0
